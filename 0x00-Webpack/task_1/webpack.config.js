54c64130491671c7ffa650c829dce7c9cfaa1479
Javan Odhiambo <114527546+Javan-Odhiambo@users.noreply.github.com>
2023-10-27 22:28:30 +0000

    Initial solution

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  mode: "production"
+};
+
+module.exports = config;
